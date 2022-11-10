df240d654cd46ccfcb4b7f4bf52727c1dfd59a79
leeant22 <leeant22@uw.edu>
2022-11-10 13:44:25 -0800

    Update

--- source/Table.R
+++ source/Table.R
@@ -2,8 +2,8 @@ seattle <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-A
 dallas <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AL-DD-ME-NK/main/data/Dallas.csv")
 new_orlean <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-AL-DD-ME-NK/main/data/NewOrleans.csv", sep = ";")
 
-seattle_gender <- seattle %>%
-  group_by(Subject_Gender) %>%
-  summarize(total_by_gender_seattle = n())
-seattle_gender <- seattle_gender[-1,]
+seattle_race <- seattle %>%
+  group_by(Subject_Race) %>%
+  summarize(total_by_race_seattle = n())
+
 
